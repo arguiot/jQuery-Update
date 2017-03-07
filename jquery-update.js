4ecbd5ff13a5d08cf19edf16b2b950e80c887a5f
Arthur Guiot <arguiot@gmail.com>
2017-03-07 17:13:23 +1100

    deleting headers

--- jquery-update.js
+++ jquery-update.js
@@ -1,6 +1,7 @@
 (function ( $ ) {
  
     $.fn.update = function(url) {
+    	var elem = $( this );
 	      var settings = $.extend({
             // These are the defaults.
             url: window.location.href
@@ -8,15 +9,15 @@
 	      $.ajax({
 	        url: url,
 	        beforeSend: function(request) {
-	            request.setRequestHeader("Access-Control-Allow-Origin", '*');
+	            // request.setRequestHeader("Access-Control-Allow-Origin", '*');
 	        },
 	        cache: false
 	      })
         .done(function (data) {
-        	var result = $(data).filter(this);
-        	this.html(result);
+        	var result = $(data).filter(elem);
+        	elem.html(result);
         });
-        return this;
+        return elem;
     };
  
 }( jQuery ));
